202b83d32e9b7264c734ab63508312feeb706731
MoochontheMic <lachlanharding2000@gmail.com>
2022-01-28 18:05:31 +0000

    failed pytest

--- naughts and crosses.py
+++ naughts and crosses.py
@@ -1,7 +1,6 @@
 import os
+import unittest
 os.system('cls')
-
-
 
 
 class Board():
@@ -31,13 +30,13 @@ class GameClass():# class for the winning conditions
     winpos3 =[2,5,8,6,7,8,8,6]
     
     def xcheck(self,array):# checks if p1 has won
-        for i in range(0,7):
+        for i in range(0,8):
             if array[self.winpos1[i]] == "X" and array[self.winpos2[i]] == "X" and array[self.winpos3[i]] == "X":
                 self.gameover = True
                 self.winner = "X"
                 continue
     def Ocheck(self,array):#checks if p2 has won
-        for i in range(0,7):
+        for i in range(0,8):
             if array[self.winpos1[i]] == "O" and array[self.winpos2[i]] == "O" and array[self.winpos3[i]] == "O":
                 self.gameover = True
                 self.winner = "O"
@@ -55,6 +54,9 @@ board = Board()
 wincheck = GameClass()
 
 print("Naughts and Crosses for Two-Players")
+
+
+
 
 
 
@@ -89,3 +91,51 @@ if wincheck.draw == True:
 else:
     print("Game winner is ", wincheck.winner)   
 #prints the result
+
+
+def TSTINPUT (array):
+    Turn = 0
+    gameover = False
+    board = Board()
+    wincheck = GameClass()
+    while wincheck.gameover == False: #loops the turns until theres a win or draw
+
+        os.system('cls')
+    
+        board.display()
+        O_choice = self.array[Turn] #p1 turn
+        board.updatecell(O_choice-1, Pieces.player1)
+        Turn += 1
+    
+        wincheck.Ocheck(board.cells)
+        if wincheck.gameover == True:
+            continue
+    #these see if the game ended after p1s turn
+        wincheck.drawcheck(Turn)
+        if wincheck.gameover == True:
+            continue
+    
+        board.display()
+        X_choice = self.array[Turn]#p2 turn
+        board.updatecell(X_choice - 1 , Pieces.player2)
+        Turn += 1
+    
+        wincheck.xcheck(board.cells)
+    #checks if the game ended after p2 turn
+    
+    board.display()
+    if wincheck.draw == True:
+       return 1
+    else:
+        if wincheck.winner == "X":
+            return 2
+        else:
+            return 3
+         
+
+
+
+class TestGame(unittest.TestCase):
+    def test_OX(self):
+        self.assert(TSTINPUT([1,4,2,5,3,6,7,8,9]),3)
+        self.assert(TSTINPUT([9,4,2,5,3,6,7,8,1]),2)
